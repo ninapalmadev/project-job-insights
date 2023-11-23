b685141fc9f69124a844079acc9f8781a772785b
Nina Palma <ninapalmadev@gmail.com>
2023-11-23 11:37:47 -0300

    Req 2: Implemente o método read

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,12 +1,16 @@
 from typing import List, Dict
-
+import csv
 
 class ProcessJobs:
     def __init__(self) -> None:
         self.jobs_list = list()
 
-    def read(self) -> List[Dict]:
-        pass
+    def read(self, msg) -> List[Dict]:
+        with open(msg, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                self.jobs_list.append(row)
+        return self.jobs_list
 
     def get_unique_job_types(self) -> List[str]:
         pass
