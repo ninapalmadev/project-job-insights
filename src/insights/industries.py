3b38949576be16760e81af4dc86eeac73a4c0ba4
Nina Palma <ninapalmadev@gmail.com>
2023-11-23 12:41:14 -0300

    [FIX] Req 5: Implemente o método get_unique_industries

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -7,9 +7,8 @@ class ProcessIndustries(ProcessJobs):
         super().__init__()
 
     def get_unique_industries(self) -> List[str]:
-        if not self.jobs_list:
-            return []
         industries = list()
         for job in self.jobs_list:
-            industries.append(job['industry'])
+            if job["industry"] != "":
+                industries.append(job["industry"])
         return list(set(industries))
